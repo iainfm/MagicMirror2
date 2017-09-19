d4fdf6c8a07910585df8fdebbedb89ca55de5e1a
iainfm <ifmclaren@gmail.com>
2017-09-19 17:27:06 +0100

    Autorun option added

---
+++ app/src/main/java/scot/mclarentech/magicmirror/RunOnStartup.java
@@ -0,0 +1,28 @@
+package scot.mclarentech.magicmirror;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+public class RunOnStartup extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
+
+            SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(context);
+            Boolean autorun = sharedPref.getBoolean("autorun_switch", true);
+            if (autorun) {
+                Intent i = new Intent(context, FullscreenActivity.class);
+                i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                context.startActivity(i);
+            }
+            else {
+                System.exit(0);
+            }
+        }
+    }
+
+}
