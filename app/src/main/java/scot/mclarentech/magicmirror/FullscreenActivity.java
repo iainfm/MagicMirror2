ef683d316dc763c31a4a6c6a15cee9cc017c12ce
iainfm <ifmclaren@gmail.com>
2017-10-09 21:58:04 +0100

    Code tidy / immersive mode

--- app/src/main/java/scot/mclarentech/magicmirror/FullscreenActivity.java
+++ app/src/main/java/scot/mclarentech/magicmirror/FullscreenActivity.java
@@ -1,7 +1,6 @@
 package scot.mclarentech.magicmirror;
 
 import android.Manifest;
-import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
@@ -18,14 +17,12 @@ import android.os.Handler;
 import android.preference.PreferenceManager;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Html;
 import android.text.SpannableString;
 import android.text.TextUtils;
 import android.text.style.RelativeSizeSpan;
 import android.util.Log;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
@@ -70,6 +67,7 @@ public class FullscreenActivity extends AppCompatActivity {
     public static TextView textViewIcon;
     public static TextView textViewWeather;
     public static TextView textViewLocation;
+    private boolean mHasFocus;
     private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
     private static final int REQUEST_FINE_LOCATION = 0;
     private Timer autoUpdate;
@@ -77,48 +75,19 @@ public class FullscreenActivity extends AppCompatActivity {
     private final Handler mHideHandler = new Handler();
     private View mContentView;
     private LinearLayout mLL_top;
-    private final Runnable mHidePart2Runnable = new Runnable() {
-        @SuppressLint("InlinedApi")
-        @Override
-        public void run() {
-            mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
-                    | View.SYSTEM_UI_FLAG_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
-        }
-    };
+
     private View mControlsView;
-
-    private final Runnable mShowPart2Runnable = new Runnable() {
-        @Override
-        public void run() {
-            // Delayed display of UI elements
-            ActionBar actionBar = getSupportActionBar();
-            if (actionBar != null) {
-                actionBar.show();
-            }
-            mControlsView.setVisibility(View.VISIBLE);
-        }
-    };
     private boolean mVisible;
-    private final Runnable mHideRunnable = new Runnable() {
-        @Override
-        public void run() {
-            hide();
-        }
-    };
 
-    private final View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
+    /* private final View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
         @Override
         public boolean onTouch(View view, MotionEvent motionEvent) {
             if (AUTO_HIDE) {
-                delayedHide(AUTO_HIDE_DELAY_MILLIS);
+                // delayedHide(AUTO_HIDE_DELAY_MILLIS);
             }
             return false;
         }
-    };
+    }; */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -134,24 +103,19 @@ public class FullscreenActivity extends AppCompatActivity {
 
         textViewWeather = (TextView) findViewById(R.id.textViewWeather);
         textViewIcon = (TextView) findViewById(R.id.textViewIcon);
-        // textViewLocation = (TextView) findViewById(R.id.textViewLocation);
-
         myThis = this;
 
         // Set up the user interaction to manually show or hide the system UI.
-        mContentView.setOnClickListener(new View.OnClickListener() {
+        /*mContentView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                toggle();
+                // toggle();
             }
-        });
+        }); */
 
-        // Upon interacting with UI controls, delay any scheduled hide()
-        // operations to prevent the jarring behavior of controls going away
-        // while interacting with the UI.
+
 
         TextView btn = (TextView) findViewById(R.id.textViewIcon);
-        // btn.bringToFront();
 
         LinearLayout mLL_top = (LinearLayout) findViewById(R.id.ll_master);
         mLL_top.bringToFront();
@@ -164,8 +128,6 @@ public class FullscreenActivity extends AppCompatActivity {
             }
         });
 
-
-        hide();
         doWeather();
         doNews();
 
@@ -177,48 +139,19 @@ public class FullscreenActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-        delayedHide(100);
-    }
-
-    private void toggle() {
-        if (mVisible) {
-            hide();
-        } else {
-            show();
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        mHasFocus = hasFocus;
+        if (mHasFocus) {
+            View decorView = getWindow().getDecorView();
+            decorView.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
         }
-    }
-
-    private void hide() {
-        // Hide UI first
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            actionBar.hide();
-        }
-        mControlsView.setVisibility(View.GONE);
-        mVisible = false;
-
-        // Schedule a runnable to remove the status and navigation bar after a delay
-        mHideHandler.removeCallbacks(mShowPart2Runnable);
-        mHideHandler.postDelayed(mHidePart2Runnable, UI_ANIMATION_DELAY);
-    }
-
-    @SuppressLint("InlinedApi")
-    private void show() {
-        // Show the system bar
-        mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
-        mVisible = true;
-
-        // Schedule a runnable to display UI elements after a delay
-        mHideHandler.removeCallbacks(mHidePart2Runnable);
-        mHideHandler.postDelayed(mShowPart2Runnable, UI_ANIMATION_DELAY);
-    }
-
-    private void delayedHide(int delayMillis) {
-        mHideHandler.removeCallbacks(mHideRunnable);
-        mHideHandler.postDelayed(mHideRunnable, delayMillis);
     }
 
     private void doWeather() {
@@ -247,8 +180,6 @@ public class FullscreenActivity extends AppCompatActivity {
 
         Resources res = getResources();
         int resID = res.getIdentifier("", "drawable", getPackageName());
-
-        // textViewLocation.setText("");
 
     }
 
@@ -290,11 +221,7 @@ public class FullscreenActivity extends AppCompatActivity {
                     CharSequence temp_loc = TextUtils.concat(temp_span, "\n", loc_span);
 
                     textViewIcon.setText(temp_loc);
-                    // textViewIcon.setGravity(CENTER);
-
-                    displayWeather = ""; // weatherResponse.getCurrently().getSummary() +
-                    // ", " + String.format("%.0f",weatherResponse.getCurrently().getTemperature()) +
-                    // "\u00B0C\n\n";
+                    displayWeather = "";
 
                 }
 
